1ff3fcdb0000070170cc4f677580909326bf42da
oaknet-code <bashir@oaknetbusiness.com>
2026-08-31 15:09:10 +0300

    Second Commmit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL configured for your local backend server
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "http://oaknetrelay.oaknetbusiness.com";
 
 // Create an Axios instance with base configuration
 const axiosInstance = axios.create({
